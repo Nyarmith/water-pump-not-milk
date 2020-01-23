6af33e9ea6c84c51186d7d32fe1775f2eb7b4051
nyarmith <powah.serge@gmail.com>
2020-01-22 23:08:05 -0500

    massive improvement and refactoring

--- coffee-machine.ino
+++ coffee-machine.ino
@@ -1,4 +1,3 @@
-#include <NewPing.h>
 #include <SoftwareSerial.h>
 
 
@@ -8,19 +7,26 @@
  * - checking that our switches are in a sane state
  */
 
-constexpr unsigned ECHO_PIN 2;
-constexpr unsigned RELAY_PIN 4;
+constexpr unsigned LOW_LEVEL_PIN 2;
+constexpr unsigned HIGH_LEVEL_PIN 3;
+constexpr unsigned FAILSAFE_LEVEL_PIN 4;
+constexpr unsigned WATER_PUMP_ENABLE_PIN 5;
+
 constexpr unsigned FILL HIGH;
 constexpr unsigned STOP LOW;
-constexpr unsigned MAX_DISTANCE 200;
-constexpr unsigned LOOP_INTERVAL 733;
+
+// TODO: Abstract these guys away
+// TODO: Make state machine with debounce time for transitions
+// TODO: Make low-pass filter
+// TODO: Implement project well (good)
+constexpr unsigned DEBOUNCE_INTERVAL_MS 733;
 constexpr unsigned MAX_FLOW_TIME 24000;
 constexpr unsigned DEBOUNCE 500;
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
 unsigned int prev_time = 0;
 unsigned int curr_time = 0;
 unsigned int flow_time = 0;
+
 void setup() {
   pinMode(TRIGGER_PIN,OUTPUT);
   pinMode(ECHO_PIN,INPUT);
@@ -30,10 +36,9 @@ void setup() {
 void loop() {
   unsigned int now = millis();
   flow_time = millis() - prev_time;
-  unsigned int distance = sonar.ping_cm();
   Serial.print(distance);
   Serial.println("cm");
-  if(debounce < LOOP_INTERVAL)
+  if(debounce < DEBOUNCE_INTERVAL_MS)
     return;
   if(distance <= 10) {
     //digitalWrite(RELAY_PIN,STOP);
@@ -45,7 +50,4 @@ void loop() {
     Serial.println("FILL!");
     prev_time = millis();  
   }                                                                                                        
-//  if(flow_time > MAX_FLOW_TIME)
-//    while(1)
-//        Serial.println("bad");
 }
